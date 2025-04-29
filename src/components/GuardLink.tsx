322acedb73ee281106d274fbc50ba6dc4f71fa8a
Tilto_ <me@tilto.kr>
2025-04-29 18:27:53 +0900

    fix: allow all props type for guardlink as

--- src/components/GuardLink.tsx
+++ src/components/GuardLink.tsx
@@ -1,45 +1,48 @@
 "use client";
 
-import Link from "next/link";
-import React, { useCallback, useRef } from "react";
-import { ComponentProps } from "react";
-import { useNavigationGuardStatus } from "../hooks/useNavigationGuardStatus";
+import Link from 'next/link';
+import React, { ComponentType, useCallback, useRef } from 'react';
+import { ComponentProps } from 'react';
+import { useNavigationGuardStatus } from 'next-naviguard';
 
-export interface GuardLinkProps extends Omit<ComponentProps<typeof Link>, "ref" | "as"> {
-    // for apply custom Link wrapper component (ex. next-i18n)
-    as?: typeof Link;
+export interface GuardLinkProps extends Omit<ComponentProps<typeof Link>, 'ref' | 'as'> {
+  // for apply custom Link wrapper component (ex. next-i18n)
+  as?: ComponentType<ComponentProps<any>>;
 }
 
 /**
  * works on next.js 15.3+
  */
-export function GuardLink({children, href, onNavigate, as, ...props}: GuardLinkProps) {
-    const linkRef = useRef<HTMLAnchorElement>(null);
-    const passGuardRef = useRef<boolean>(false);
-    const LinkComp = as || Link;
+export function GuardLink({ children, href, onNavigate, as, ...props }: GuardLinkProps) {
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const passGuardRef = useRef<boolean>(false);
+  const LinkComp = as || Link;
 
-    const { isGuardActive, dryGuard } = useNavigationGuardStatus({
-        to: href.toString()
-    });
-    
-    const handleNavigate = useCallback(async (e: {preventDefault: () => void}) => {
-        if (isGuardActive() && !passGuardRef.current) {
-            e.preventDefault();
-            const blocked = await dryGuard();
-            if (!blocked) {
-                onNavigate?.(e);
-                passGuardRef.current = true;
-                linkRef.current?.click();
-            }
-            return;
-        } else {
-            onNavigate?.(e);
+  const { isGuardActive, dryGuard } = useNavigationGuardStatus({
+    to: href.toString(),
+  });
+
+  const handleNavigate = useCallback(
+    async (e: { preventDefault: () => void }) => {
+      if (isGuardActive() && !passGuardRef.current) {
+        e.preventDefault();
+        const blocked = await dryGuard();
+        if (!blocked) {
+          onNavigate?.(e);
+          passGuardRef.current = true;
+          linkRef.current?.click();
         }
-    }, [isGuardActive, onNavigate, dryGuard]);
+        return;
+      } else {
+        onNavigate?.(e);
+      }
+    },
+    [isGuardActive, onNavigate, dryGuard],
+  );
 
-    return (
-        <LinkComp href={href} onNavigate={handleNavigate} ref={linkRef} {...props}>
-            {children}
-        </LinkComp>
-    );
+  return (
+    <LinkComp href={href} onNavigate={handleNavigate} ref={linkRef} {...props}>
+      {children}
+    </LinkComp>
+  );
 }
